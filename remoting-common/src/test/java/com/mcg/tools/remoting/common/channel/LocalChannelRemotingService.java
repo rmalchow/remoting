6107ab61e98e84665705cfb0e1ab523b35fa5358
Ruben Malchow <ruben.malchow@mcon-group.com>
2019-04-05 11:30:38 +0200

    fix test

--- remoting-common/src/test/java/com/mcg/tools/remoting/common/channel/LocalChannelRemotingService.java
+++ remoting-common/src/test/java/com/mcg/tools/remoting/common/channel/LocalChannelRemotingService.java
@@ -1,10 +1,8 @@
 package com.mcg.tools.remoting.common.channel;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import com.mcg.tools.remoting.api.RemotingInterceptor;
 import com.mcg.tools.remoting.common.AbstractRemotingService;
 import com.mcg.tools.remoting.common.ExportedService;
 import com.mcg.tools.remoting.common.io.ClientChannel;
