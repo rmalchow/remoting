d2c3a098ced47a6f20017964040d8c589afe99d8
Ruben Malchow <ruben.malchow@mcon-group.com>
2020-08-07 17:10:45 +0200

    more agressive re-listen and low level recovery

--- remoting-impl-amqp/src/main/java/com/mcg/tools/remoting/impl/amqp/AmqpServerChannelProvider.java
+++ remoting-impl-amqp/src/main/java/com/mcg/tools/remoting/impl/amqp/AmqpServerChannelProvider.java
@@ -17,9 +17,13 @@ import org.springframework.stereotype.Component;
 import com.mcg.tools.remoting.common.ExportedService;
 import com.mcg.tools.remoting.common.interfaces.ServerChannel;
 import com.mcg.tools.remoting.common.interfaces.ServerChannelProvider;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.ExceptionHandler;
+import com.rabbitmq.client.TopologyRecoveryException;
 
 @Component
-public class AmqpServerChannelProvider implements ServerChannelProvider, ConnectionListener {
+public class AmqpServerChannelProvider implements ServerChannelProvider, ConnectionListener, ExceptionHandler {
 	
 	private static Log log = LogFactory.getLog(AmqpServerChannelProvider.class);
 
@@ -30,6 +34,8 @@ public class AmqpServerChannelProvider implements ServerChannelProvider, Connect
 	
 	private Connection connection;
 
+	private ExceptionHandler parentHandler;
+	
 	@Override
 	public ServerChannel createServerChannel(String app, String service, ExportedService exportedService) {
 		AmqpServerChannel s = new AmqpServerChannel(app, service, exportedService);
@@ -64,9 +70,84 @@ public class AmqpServerChannelProvider implements ServerChannelProvider, Connect
 	public void init() {
 		CachingConnectionFactory ccf = (CachingConnectionFactory)connectionFactory;
 		com.rabbitmq.client.ConnectionFactory cf = ccf.getRabbitConnectionFactory();
+		parentHandler = cf.getExceptionHandler();
 		cf.setAutomaticRecoveryEnabled(true);
 		cf.setTopologyRecoveryEnabled(true);
+		cf.setExceptionHandler(this);
 		ccf.addConnectionListener(this);
 		ccf.createConnection();
+	}
+
+	@Override
+	public void handleUnexpectedConnectionDriverException(com.rabbitmq.client.Connection conn, Throwable exception) {
+		try {
+			this.connection.close();
+		} catch (Exception e) {
+		}
+		try {
+			init();
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public void handleReturnListenerException(Channel channel, Throwable exception) {
+		// TODO Auto-generated method stub
+		parentHandler.handleReturnListenerException(channel, exception);
+	}
+
+	@Override
+	public void handleConfirmListenerException(Channel channel, Throwable exception) {
+		// TODO Auto-generated method stub
+		parentHandler.handleConfirmListenerException(channel, exception);
+	}
+
+	@Override
+	public void handleBlockedListenerException(com.rabbitmq.client.Connection connection, Throwable exception) {
+		// TODO Auto-generated method stub
+		parentHandler.handleBlockedListenerException(connection, exception);
+	}
+
+	@Override
+	public void handleConsumerException(Channel channel, Throwable exception, Consumer consumer, String consumerTag,
+			String methodName) {
+		// TODO Auto-generated method stub
+		parentHandler.handleConsumerException(channel, exception, consumer, consumerTag, methodName);
+	}
+
+	@Override
+	public void handleConnectionRecoveryException(com.rabbitmq.client.Connection conn, Throwable exception) {
+		try {
+			this.connection.close();
+		} catch (Exception e) {
+		}
+		try {
+			init();
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public void handleChannelRecoveryException(Channel ch, Throwable exception) {
+		try {
+			this.connection.close();
+		} catch (Exception e) {
+		}
+		try {
+			init();
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public void handleTopologyRecoveryException(com.rabbitmq.client.Connection conn, Channel ch, TopologyRecoveryException exception) {
+		try {
+			this.connection.close();
+		} catch (Exception e) {
+		}
+		try {
+			init();
+		} catch (Exception e) {
+		}
 	}
 }
